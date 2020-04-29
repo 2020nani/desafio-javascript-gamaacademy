8cafa7791d7cb6bd5b35a035ab6d08b7a7a9625a
hernani <megahack9@outlook.com>
2020-04-28 22:08:08 -0300

    function (validacao) complety

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 console.log("Javascript carregado")
-
+function validaCPF(cpf){
+return false
+}
 function validacao(){
   console.log("Iniciando validacao")
+  //buscando dados digitados na pagina html
+  var cpf = document.getElementById('cpf_digitado').value;
+  //validando cpf digitado pelo usuario
+  var resultadoValidacao = validaCPF(cpf);
+  if(resultadoValidacao){
+    document.getElementById('success').style.display = 'block';
+  }
+  else{
+    document.getElementById('error').style.display = 'block';
+  }
 }
