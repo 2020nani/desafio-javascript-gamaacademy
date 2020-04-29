9385b244fbe205cac7db797c236a1763a75cd8e5
hernani <megahack9@outlook.com>
2020-04-28 22:44:58 -0300

    first digit cpf validation

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 console.log("Javascript carregado")
 function validaCPF(cpf){
-return false
+ if(cpf.length != 11){
+   return false;
+ }
+ else{
+   var numeros = cpf.substring(0,9);
+   var digitos = cpf.substring(9);
+   var soma = 0;
+   for(var i = 10; i>1; i--){
+       soma += numeros.charAt(10-i) * i; 
+   }
+  
+   var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+   //validacao primeiro digito
+   if(resultado != digitos.charAt(0)){
+     return false;
+   }
+  
+   return true;
+ }
 }
 function validacao(){
   console.log("Iniciando validacao")
