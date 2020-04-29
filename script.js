d129c49ed5b980ff7433fa54e7ff8a35ff181e8d
hernani <megahack9@outlook.com>
2020-04-28 23:05:45 -0300

    complete cpf validation

--- script.js
+++ script.js
@@ -4,24 +4,41 @@ function validaCPF(cpf){
    return false;
  }
  else{
+    //validacao primeiro digito
    var numeros = cpf.substring(0,9);
    var digitos = cpf.substring(9);
    var soma = 0;
    for(var i = 10; i>1; i--){
        soma += numeros.charAt(10-i) * i; 
    }
-  
+   
    var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-   //validacao primeiro digito
+  
    if(resultado != digitos.charAt(0)){
      return false;
    }
+  //validacao segundo digito 
+   soma = 0;
+   numeros = cpf.substring(0, 10);
+
+for(var k = 11; k>1; k--){
+       soma += numeros.charAt(11-k) * k; 
+   }
+
+    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
   
+   if(resultado != digitos.charAt(1)){
+     return false;
+   }
+
    return true;
  }
 }
 function validacao(){
   console.log("Iniciando validacao")
+  document.getElementById('success').style.display = 'none';
+  
+  document.getElementById('error').style.display = 'none';
   //buscando dados digitados na pagina html
   var cpf = document.getElementById('cpf_digitado').value;
   //validando cpf digitado pelo usuario
